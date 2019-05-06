3096a1460c9d1ce78b23ca120fbfec34570db051
BiancaIlie <biailie@yahoo.com>
2019-05-06 20:58:02 +0300

    Ex2

---
+++ src/Ex2.java
@@ -0,0 +1,22 @@
+public class Ex2{
+    public static void main(String[] args){
+
+        int n = 6;
+        int counter=1;
+        System.out.println("N=" +n);
+        do {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            System.out.println("N=" +n);
+            counter++;
+
+        } while (n!=1);
+
+        System.out.println(counter);
+
+    }
+
+}
--- src/Testttt.java
+++ src/Testttt.java
@@ -1,5 +1,5 @@
-public class Testttt {
-    public static void main(String[] args){
-        String[]masini new String[5]
-    }
-}
+//public class Testttt {
+//    public static void main(String[] args){
+//        String[]masini new String[5]
+//    }
+//}
