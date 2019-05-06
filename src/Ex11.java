3c938621a41d8945e71f316c967a121b4caf55fe
BiancaIlie <biailie@yahoo.com>
2019-05-06 22:13:14 +0300

    Ex11

---
+++ src/Ex11.java
@@ -0,0 +1,30 @@
+public class Ex11 {
+    public static void main (String[] args){
+        int n = 5;
+        String numar=null;
+        if (n==0) {
+            numar= "zero";
+        } else if (n==1) {
+            numar = "unu";
+        } else if (n==2) {
+            numar = "doi";
+        } else if (n==3) {
+            numar = "trei";
+        } else if (n==4) {
+            numar = "patru";
+        } else if (n==5) {
+            numar = "cinci";
+        } else if (n==6) {
+            numar = "sase";
+        } else if (n==7) {
+            numar = "sapte";
+        } else if (n==8) {
+            numar = "opt";
+        } else if (n==9) {
+            numar = "noua";
+        }
+
+        System.out.println(numar);
+
+    }
+}
